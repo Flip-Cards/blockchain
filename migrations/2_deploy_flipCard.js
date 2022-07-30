a7f9755b84d0a8ffa395a0acb76cf36893cabd3f
aashishpeepra <ironhack082327@gmail.com>
2022-07-30 17:17:25 +0530

    Ade

--- migrations/2_deploy_flipCard.js
+++ migrations/2_deploy_flipCard.js
@@ -1,5 +1,12 @@
 const Contacts = artifacts.require("flipCard.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Contacts);
+    deployer.deploy(Contacts)
+
+    // Option 2) Console log the address:
+    .then(() => console.log(SimpleStorage.address))
+
+    // Option 3) Retrieve the contract instance and get the address from that:
+    .then(() => SimpleStorage.deployed())
+    .then(_instance => console.log(_instance.address));
 };
