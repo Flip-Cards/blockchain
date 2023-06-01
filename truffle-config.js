030c942f346dfd212459b7a1bfe2e09b04405fa4
= <ironhack082327@gmail.com>
2023-06-02 01:40:10 +0530

    updated coce

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,20 @@
+const MNEMONIC = "rare win chase uncle crop video betray indoor work cup banana fluid"
+const API_KEY = "https://ropsten.infura.io/v3/d9d6d74ce9cf4bf299fb2e2a21449229"
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, API_KEY)
+      },
+      network_id: 3,
+      gas: 30000000      //make sure this gas allocation isn't over 4M, which is the max
     }
   },
   compilers: {
